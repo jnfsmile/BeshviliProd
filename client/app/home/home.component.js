b3f57c0da926a5de56fe230540bff9f9c83fe4ed
JNF <jnfsmile@gmail.com>
2017-02-13 21:41:41 +0200

    add newrelic

--- client/app/home/home.component.js
+++ client/app/home/home.component.js
@@ -15,11 +15,8 @@ var HomeComponent = (function () {
     HomeComponent = __decorate([
         core_1.Component({
             selector: 'home',
-            // We need to tell Angular's Dependency Injection which providers are in our app.
             providers: [],
-            // Our list of styles in our component. We may add more to compose many styles together
             styles: [],
-            // Every Angular template is first compiled by the browser before Angular runs it's compiler
             templateUrl: '/app/home/home.html'
         }), 
         __metadata('design:paramtypes', [])
--- client/app/notYet/notyet.component.js
+++ client/app/notYet/notyet.component.js
@@ -9,34 +9,14 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-/*
- * We're loading this component asynchronously
- * We are using some magic with es6-promise-loader that will wrap the module with a Promise
- * see https://github.com/gdi2290/es6-promise-loader for more info
- */
 console.log('`NotYet` component loaded asynchronously');
 var NotYetComponent = (function () {
     function NotYetComponent() {
     }
     NotYetComponent.prototype.ngOnInit = function () {
         console.log('hello `NotYet` component');
-        // static data that is bundled
-        //var mockData = require('assets/mock-data/mock-data.json');
-        //console.log('mockData', mockData);
-        // if you're working with mock data you can also use http.get('assets/mock-data/mock-data.json')
-        //this.asyncDataWithWebpack();
     };
     NotYetComponent.prototype.asyncDataWithWebpack = function () {
-        // you can also async load mock data with 'es6-promise-loader'
-        // you would do this if you don't want the mock-data bundled
-        // remember that 'es6-promise-loader' is a promise
-        //var asyncMockDataPromiseFactory = require('es6-promise!assets/mock-data/mock-data.json');
-        //setTimeout(() => {
-        //  let asyncDataPromise = asyncMockDataPromiseFactory();
-        //  asyncDataPromise.then(json => {
-        //    console.log('async mockData', json);
-        //  });
-        //});
     };
     NotYetComponent = __decorate([
         core_1.Component({
